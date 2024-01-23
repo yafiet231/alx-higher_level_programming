0e953c5592a08ed82c8e8b361b13353d8d82e1eb
yafiet231 <abieleyasu893@gmail.com>
2024-01-23 12:31:10 -0800

    5-square.py

---
+++ 0x06-python-classes/5-main.py
@@ -0,0 +1,18 @@
+guillaume@ubuntu:~/0x06$ cat 5-main.py
+#!/usr/bin/python3
+Square = __import__('5-square').Square
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
