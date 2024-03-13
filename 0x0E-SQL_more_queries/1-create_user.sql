790937e82f4e0d7ff8b7bfb0713a844bc6a62fb3
yafiet231 <abieleyasu893@gmail.com>
2024-03-13 10:10:19 -0700

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- creates the MySQL server user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- grant all privileges
+GRANT ALL PRIVILEGES
+ON *.* TO 'user_0d_1'@'localhost';
